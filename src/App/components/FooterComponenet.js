a5563c58032b6075de264dbbf25b14d8e27bdbeb
Luc van Kessel <lucv.k@outlook.com>
2022-05-11 22:32:57 +0200

    small footer edit

--- src/App/components/FooterComponenet.js
+++ src/App/components/FooterComponenet.js
@@ -1,7 +1,7 @@
 export default function FooterComponent() {
     return(
         <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://flowbite.com" class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://lucvkessel.nl" class="hover:underline">Luc van Kessel</a>. All Rights Reserved.
             </span>
             <ul class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li>
