b97ee1e938874efeaf7ceb0128b593b8e78371bc
Luc van Kessel <lucv.k@outlook.com>
2022-05-13 14:50:52 +0200

    added structured data to the app instead of the index.html directly

--- src/App/index.js
+++ src/App/index.js
@@ -6,16 +6,39 @@ import ExperienceSection from './sections/ExperienceSection';
 import ProjectsSection from './sections/ProjectsSection';
 import SkillsSection from './sections/SkillsComponent';
 
+const personStructuredData = {
+    "@context": "https://schema.org/",
+    "@type": "Person",
+    "name": "Luc van Kessel",
+    "alternateName": "Lucas van Kessel",
+    "nationality": "Dutch",
+    "description": "Student SoftwareEngineering Hanzehogeschool Groningen",
+    "url": "https://lucvkessel.nl",
+    "email": "lucv.k@outlook.com",
+    "sameAs": 
+    [
+        "https://www.linkedin.com/in/luc-van-kessel-1448a71a2",
+        "https://github.com/lucvankessel"
+    ]
+};
+
 function App() {
     return (
-        <div className='bg-slate-800 text-slate-400'>
-            <IntroComponenet />
-            <AboutSecton />
-            <SkillsSection />
-            <EducationSection />
-            <ExperienceSection />
-            <ProjectsSection />
-            <FooterComponent />
+        <div>
+
+            <script type="application/ld+json">
+                {JSON.stringify(personStructuredData)}
+            </script>
+
+            <div className='bg-slate-800 text-slate-400'>
+                <IntroComponenet />
+                <AboutSecton />
+                <SkillsSection />
+                <EducationSection />
+                <ExperienceSection />
+                <ProjectsSection />
+                <FooterComponent />
+            </div>
         </div>
     );
 }
