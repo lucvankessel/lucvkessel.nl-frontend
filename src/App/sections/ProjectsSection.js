0dd53e9cc22bdd0d913a0e84e8e1555ce4695167
Luc van Kessel <lucv.k@outlook.com>
2023-02-27 22:08:37 +0100

    added internship project

--- src/App/sections/ProjectsSection.js
+++ src/App/sections/ProjectsSection.js
@@ -5,6 +5,15 @@ function Content() {
         <div>
             
             <ProjectItem 
+                title='Generieke async endpoints'
+                description='   Aan dit project werk ik tijdens mijn stage bij klippa. Het bestaat uit het uitbreiden van het DocHorizon product met API eindpoints die async gegevens moeten kunnen ontvangen en terug geven.
+                                Dit gebeurt door het maken van queue workers die alle logica op de achterkant uitvoeren en de klant deze kunnen opvragen zodra deze klaar is.
+                                Ook is er de mogelijkheid om wehooks te gebruiken zodat de klant zelf niet hoeft bij te houden waneer haar data klaar is.'
+                tech={['Golang', 'Asynq(redis)', 'Ent', 'Minio', 'webhooks', 'gin/fizz']}
+                link='https://www.klippa.com/en/dochorizon/'
+            />
+
+            <ProjectItem 
                 title='Persoonlijke website'
                 description='   Dit project kijkt u nu naar. Mijn CV in website vorm. Gemaakt met ReactJS en TailwindCSS. 
                                 Gehost in een Docker container op DigitalOcean. 
