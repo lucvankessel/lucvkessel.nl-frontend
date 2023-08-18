1b19548bde56be27151df816c62cadaf95fe5d26
Luc van Kessel <lucv.k@outlook.com>
2023-08-18 21:58:44 +0200

    updated year in footer

--- src/App/components/FooterComponenet.js
+++ src/App/components/FooterComponenet.js
@@ -2,7 +2,7 @@ export default function FooterComponent() {
     return(
         <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6" itemScope itemType="http://schema.org/Person">
             <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                Luc van Kessel, 2022
+                Luc van Kessel, 2023
             </span>
             <div class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <a className="mx-2" href="https://www.linkedin.com/in/luc-van-kessel-1448a71a2">Linkedin</a>
