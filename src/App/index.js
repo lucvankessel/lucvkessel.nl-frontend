f147cf9f75edcf1b9c70f7b29646119d0c690dd9
Luc van Kessel <lucv.k@outlook.com>
2023-12-04 21:54:36 +0100

    IntroComponent spelling fixed, removed old schema that did not seem to work

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import IntroComponenet from './components/IntroComponent';
+import IntroComponent from './components/IntroComponent';
 import FooterComponent from './components/FooterComponenet';
 import AboutSecton from './sections/AboutSection';
 import EducationSection from './sections/EducationSection';
@@ -6,32 +6,12 @@ import ExperienceSection from './sections/ExperienceSection';
 import ProjectsSection from './sections/ProjectsSection';
 import SkillsSection from './sections/SkillsComponent';
 
-const personStructuredData = {
-    "@context": "http://schema.org/",
-    "@type": "Person",
-    "name": "Luc van Kessel",
-    "alternateName": "Lucas van Kessel",
-    "nationality": "Dutch",
-    "description": "Student SoftwareEngineering Hanzehogeschool Groningen",
-    "url": "https://lucvkessel.nl",
-    "email": "lucv.k@outlook.com",
-    "sameAs": 
-    [
-        "https://www.linkedin.com/in/luc-van-kessel-1448a71a2",
-        "https://github.com/lucvankessel"
-    ]
-};
-
 function App() {
     return (
         <div>
 
-            <script type="application/ld+json">
-                {JSON.stringify(personStructuredData)}
-            </script>
-
             <div className='bg-slate-800 text-slate-400'>
-                <IntroComponenet />
+                <IntroComponent />
                 <AboutSecton />
                 <SkillsSection />
                 <EducationSection />
