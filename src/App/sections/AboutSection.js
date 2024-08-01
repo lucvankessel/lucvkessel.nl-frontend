56fb86583e02991b28075d0e42b67f8bdbec30a1
Luc van Kessel <lucv.k@outlook.com>
2024-08-01 19:09:53 +0200

    updated about me section

--- src/App/sections/AboutSection.js
+++ src/App/sections/AboutSection.js
@@ -3,10 +3,10 @@ import DetailSectionComponent from "../components/DetailSectionComponent";
 function Content() {
     return (
         <p className="max-w-2xl">
-            Op het moment ben ik bezig met de bachelor HBO-ICT aan de Hanzehogeschool, met als bachelor SoftwareEngineering. 
-            Mijn hobbies zijn onder anderen: sporten, programmeren, gamen en natuurlijk leuke dingen doen met vrienden. 
-            Ik zou mijzelf beschrijven als een gedreven student/werknemer die graag extra dingen bijleert en mee denkt aan hoe processen simpeler, 
-            efficiënter of vriendelijker kunnen worden voor andere werknemers of klanten.
+            Recentelijk heb ik een bachelor software engineering afgerond aan de Hanzehogeschool en start in September 2024 met een pre-master computer science.
+            Mijn hobbies zijn onder anderen: sporten(sportschool, kitesurfen, snowboarden), programmeren en ben veel te vinden met vrienden op festivals of waar dan ook. 
+            Ik zou mijzelf omschrijven als een gedreven student/werknemer die graag extra dingen bijleert en mee denkt aan hoe processen simpeler, 
+            efficiënter of vriendelijker kunnen worden voor klanten en andere werknemers.
         </p>
     );
 }
