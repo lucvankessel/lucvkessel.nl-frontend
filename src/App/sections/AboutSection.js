0b17728fec2f1e185cdec47d9e8420cdac9026da
Luc van Kessel <lucv.k@outlook.com>
2025-06-28 12:01:56 +0200

    Updated various sections due to now working full-time and not being a student anymore

--- src/App/sections/AboutSection.js
+++ src/App/sections/AboutSection.js
@@ -3,7 +3,7 @@ import DetailSectionComponent from "../components/DetailSectionComponent";
 function Content() {
     return (
         <p className="max-w-2xl">
-            Recentelijk heb ik een bachelor software engineering afgerond aan de Hanzehogeschool en start in September 2024 met een pre-master computer science.
+            Ik werk op het moment voor Klippa als een backend programmeur aan het SpendControl product.
             Mijn hobbies zijn onder anderen: sporten(sportschool, kitesurfen, snowboarden), programmeren en ben veel te vinden met vrienden op festivals of waar dan ook. 
             Ik zou mijzelf omschrijven als een gedreven student/werknemer die graag extra dingen bijleert en mee denkt aan hoe processen simpeler, 
             efficiënter of vriendelijker kunnen worden voor klanten en andere werknemers.
